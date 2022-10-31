9e4c78c29023c66a525a2b91b6886e50c11ae206
praveenkumarj27 <jpraveenkumar263@gmail.com>
2022-10-31 10:33:02 +0530

    init

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ const mongodb = require('mongodb')
 const dotenv=require('dotenv').config()
 const dbName = process.env.DB_NAME;
 const MongoClient=mongodb.MongoClient;
-const dbUrl=`${process.env.URL}/${process.env.DB_NAME}`
+const dbUrl=`${process.env.DB_URL}/${process.env.DB_NAME}`
 
 
 
